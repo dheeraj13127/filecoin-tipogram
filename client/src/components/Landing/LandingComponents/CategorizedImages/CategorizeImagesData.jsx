977ffccf88d5726ba46ae027520fc82dccae3318
dheeraj13127 <dheerajkumar13127@gmail.com>
2022-07-15 00:39:25 +0530

    added categorized section

---
+++ client/src/components/Landing/LandingComponents/CategorizedImages/CategorizeImagesData.jsx
@@ -0,0 +1,23 @@
+import digitalcat from '../../../../assets/landing/digitalcat.png';
+import dk from '../../../../assets/landing/dk.png';
+import shibha from '../../../../assets/landing/shibha.png';
+
+
+export const categorizedImagesData = [
+    {
+        id:1,
+        url:digitalcat,
+        type:"🎨 Art"
+    },
+    {
+        id:2,
+        url:dk,
+        type:"🏏 Sport"
+    },
+    {
+        id:3,
+        url:shibha,
+        type:"🐼 cartoon"
+    }
+
+]
---
+++ client/src/components/Landing/LandingComponents/CategorizedImages/CategorizedImages.jsx
@@ -0,0 +1,27 @@
+import { Button, Grid, Typography } from '@mui/material'
+import React from 'react'
+import '../../../../styles/LandingStyles/CategorizedImages.scss'
+import { categorizedImagesData } from './CategorizeImagesData'
+function CategorizedImages() {
+  return (
+    <div className='categorizedImagesContainer'>
+        <Grid container>
+        <Grid item xs={12}>
+                <Typography variant='h3' className='catergorizedImagesHeader'>Categorized Images</Typography>
+            </Grid>
+     
+      {
+        categorizedImagesData.map((imgs,index)=>(
+          <Grid item xs={12} sm={12} md={4} lg={4} className="categorizedImagesBox">
+            <Button className='categorizedImagesTypeBtn'>{imgs.type}</Button>
+            <img src={imgs.url} alt="categorized-images" className='categorizedImagesImg'/>
+          </Grid>
+        ))
+      }
+     
+        </Grid>
+    </div>
+  )
+}
+
+export default CategorizedImages

ad48599bc0caa024b9279d85d7ae63d5c391155d
dheeraj13127 <dheerajkumar13127@gmail.com>
2022-07-15 01:55:49 +0530

    fixed a bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BlockchainProvider from './blockchain';
-import { Dashboard, Landing } from './globals';
+import { Dashboard, Landing, PageNotFound } from './globals';
 import './App.scss';
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing/>} />
         <Route path='/dashboard' element={<Dashboard/>}/>
-       
+        <Route path='*' element={<PageNotFound/>} />
       </Routes>
     </Router>
     </>

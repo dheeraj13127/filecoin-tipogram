876e6fa2526a2c16825727824693f1c711ff9ca6
dheeraj13127 <dheerajkumar13127@gmail.com>
2022-08-07 20:04:43 +0530

    small bug fix in backedn

--- server/index.js
+++ server/index.js
@@ -16,6 +16,11 @@ mongoose.connect(process.env.DATABASE,{
 .catch(err=>console.log(err))
 app.use('/auth',authRoutes)
 app.use('/dashboard',dashboardRoutes)
-
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 const PORT=process.env.PORT||5000
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
